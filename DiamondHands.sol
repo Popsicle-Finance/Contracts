190acc4bf59a54695aec0c09a3fd8c55dcd060f0
PopsicleFinance <80819254+PopsicleFinance@users.noreply.github.com>
2021-03-19 12:54:40 +0200

    Added ReentrancyGuard for withdrawal

--- DiamondHands.sol
+++ DiamondHands.sol
@@ -420,6 +420,65 @@ library SafeERC20 {
 }
 
 /**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor () {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+/**
  * This contract is made to reward ICE token holder
  * who are willing to stake their $ICE hence supporting 
  * the jump start of popsicle.finance
@@ -432,7 +491,7 @@ library SafeERC20 {
  * before the end of the contract
  */
 
-contract DiamondHands is Ownable {
+contract DiamondHands is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
     
     // unlock time when the funds are available for withdrawal
@@ -444,7 +503,7 @@ contract DiamondHands is Ownable {
     // total amount of ICE token that users deposited to this contract
     uint256 public totalDeposited = 0;
     
-    uint256 week = 1 minutes;
+    uint256 week = 1 weeks;
 
     // ICE Token pointer
     IERC20 immutable public iceToken;
@@ -500,7 +559,7 @@ contract DiamondHands is Ownable {
     
     // Withdraw ICE tokens from DiamondHands. SafeRewardTransfer gets triggered for pendingReward
     // Can be called only after the staking period is finished
-    function withdraw() external {
+    function withdraw() external nonReentrant {
         require(block.timestamp >= unlockTime, "Withdraw is not unlocked");
         UserInfo storage user = userInfo[msg.sender];
         uint256 pendingTokenReward = pendingReward(msg.sender);
